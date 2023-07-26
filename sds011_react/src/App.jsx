c8695772dcf078cb8590b68cb2846d50980691e8
printfinn <git@printf.win>
2023-07-26 17:10:16 +0800

    Let React query Flask API.

--- sds011_react/src/App.jsx
+++ sds011_react/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import chipseeLogo from '/chipsee.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pm25, setPm25] = useState("N/A")
+
+  // Poll the Python server every 3 seconds.
+  useEffect(() => {
+    const queryInterval = setInterval(() => {
+      fetch("http://127.0.0.1:5000", {mode: "cors"})
+      .then((response) => response.json())
+      .then((result) => setPm25(result["pm25"]))
+      .catch((error) => setPm25("N/A"))
+    }, 3000);
+
+    return () => clearInterval(queryInterval);
+  }, []);
 
   return (
     <>
@@ -15,19 +28,16 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://www.chipsee.com" target="_blank">
+          <img src={chipseeLogo} className="logo" alt="Chipsee logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Chipsee</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          PM2.5 is {pm25}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
